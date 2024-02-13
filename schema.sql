65d9d29e95f50b5ac465f9f3c13beeb6a93c690b
Cameron Williams <cswilliams112004@gmail.com>
2024-02-13 15:13:14 -0500

    initial commit

---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE  apple_ceo(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ceo_Name TEXT,
+    slug TEXT,
+    first_Year INTEGER,
+);
---
+++ seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO apple_ceo (ceo_Name, slug, first_Year)
+VALUES  ('Michael Spindler', 'michael spindler', 1993),
+        ('Gil Amelio', 'gil amelio', 1996),
+        ('Mike Markkula', 'mike markkula', 1977),
+        ('Tim Cook', 'tim-cook', 2011),
+        ('Mike Scott', 'mike scott', 1978),
+        ('John Sculley', 'john sculley', 1983),
+        ('Steve Jobs', 'steve jobs', 1976)
+        ('Steve Jobs', 'steve jobs', 1997);
